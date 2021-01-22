c9bb9efac9a48a07cdbf9b026aa0ee4bad97243b
India Kato <india.kato@gmail.com>
2021-01-22 09:58:58 -0800

    create method

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -21,6 +21,16 @@ class VideosController < ApplicationController
       )
   end
 
+  def create
+    video = Video.new(video_params)
+
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created
+    else
+      render json: { errors: video.errors.messages }, status: :bad_request
+    end
+  end
+
   private
 
   def require_video
@@ -28,5 +38,9 @@ class VideosController < ApplicationController
     unless @video
       render status: :not_found, json: { errors: { title: ["No video with title #{params["title"]}"] } }
     end
+  end
+
+  def video_params
+    return params.permit(:title, :overview, :release_date, :inventory, :available_inventory)
   end
 end
